e3a338b89e948be101c1f5a0896336db4de607ac
Diana O'Haver <c4t3rp1ll4r@gmail.com>
2015-06-23 17:03:03 -0500

    Added larger bank of default cities, cleaned up var names

--- config.js
+++ config.js
@@ -2,28 +2,107 @@ var Twit = require('twit');
 var Weather = require('weather-js');
 
 var T = new Twit({
-   
+
 });
 
 var defaultList = [
-		'San Jose, CA',	// san jose
-		'Seattle, WA',	// seattle
-		'Portland, OR',	// portland
-		'Memphis, TN',	// memphis
-		'Fort Worth, TX',	// fort worth
-		'Sacramento, CA',	// sacramento
-		'Colorado Springs, CO',	// colorado springs
-		'Springfield, MO',	// springfield
-		'Lowell, MA',	// lowell
-		'Oklahoma City, OK'		// oklahoma city
+		'San Jose, CA',	
+		'Seattle, WA',	
+		'Portland, OR',	
+		'Memphis, TN',	
+		'Fort Worth, TX',	
+		'Sacramento, CA',	
+		'Colorado Springs, CO',	
+		'Springfield, MO',	
+		'Lowell, MA',	
+		'Oklahoma City, OK', 		
+		'Baltimore, MD',
+		'Jacksonville, FL',
+		'Tuscon, AZ',
+		'Little Rock, AR',
+		'El Paso, TX',
+		'San Antonio, TX',
+		'Reno, NV',
+		'Salt Lake City, UT',
+		'San Diego. CA',
+		'Albuquerque, NM',
+		'Kansas City, MO',
+		'Saint Louis, MO',
+		'San Francisco, CA',
+		'Oakland, CA',
+		'Los Angeles, CA',
+		'Long Beach, CA',
+		'Anaheim, CA',
+		'Phoenix, AZ',
+		'Spokane, WA',
+		'Las Vegas, NV',
+		'Boisie, ID',
+		'Billings, MT',
+		'Denver, CO',
+		'Santa Fe, NM',
+		'Houston, TX',
+		'Austin, TX',
+		'Dallas, TX',
+		'Tulsa, OK',
+		'Wichita, KS',
+		'Omaha, NE',
+		'Des Moines, IA',
+		'Sioux Falls, SD',
+		'Fargo, ND',
+		'Saint Paul, MN',
+		'Minneapolis, MN',
+		'Milwaukee, WI',
+		'Chicago, IL',
+		'New Orleans, LA',
+		'Jackson, MS',
+		'Birmingham, AL',
+		'Tampa, FL',
+		'Miami, FL',
+		'Orlando, FL',
+		'Atlanta, GA',
+		'Nashville, TN',
+		'Charlotte, NC',
+		'Charleston, SC',
+		'Louisville, KY',
+		'Lexington, KY',
+		'Indianapolis, IN',
+		'Cleveland, OH',
+		'Cincinatti, OH',
+		'Detroit, MI',
+		'Virginia Beach, VA',
+		'Washington, DC',
+		'Philadelphia, PA',
+		'Pittsburgh, PA',
+		'Newark, NJ',
+		'Dover, DE',
+		'New York, NY',
+		'Buffalo, NY',
+		'Hartford, CT',
+		'Providence, RI',
+		'Boston, MA',
+		'Portland, ME',
+		'Toronto, ON',
+		'Ottawa, ON',
+		'Montreal, QC', 
+		'Quebec City, QC',
+		'Edmonton, AB',
+		'Calgary, AB',
+		'Vancouver, BC',
+		'Victoria, BC',
+		'Whitehorse, YT',
+		'Yellowknife, NT',
+		'Winnipeg, MB',
+		'Regina, SK',
+		'Halifax, NS'
+
 	];
 
-	var rand = Math.floor((Math.random() * 9) + 1);
+	var rand = Math.floor((Math.random() * 88) + 1);
 
 	var city = defaultList[rand];
 
 	var weathering;
-	var city;
+	var thecity;
 	var temp;
 	var desc;
 
@@ -34,23 +113,23 @@ Weather.find({search: city, degreeType: 'F'}, function(err, result) {
 	weathering = result;
 
 	// convert to string
-	city = JSON.stringify(weathering[0].location.name);
+	thecity = JSON.stringify(weathering[0].location.name);
 	temp = JSON.stringify(weathering[0].current.temperature);
 	desc = JSON.stringify(weathering[0].current.skytext);
 
 	// get rid of quotation marks
-	city = city.replace(/\"/g, "");
+	thecity = city.replace(/\"/g, "");
 	temp = temp.replace(/\"/g, "");
 	desc = desc.replace(/\"/g, "");
 
 	// convert description to lower case since it goes at the end of the tweet
 	desc = desc.toLowerCase();
 
-	console.log(city);
+	console.log(thecity);
 	console.log(temp);
 	console.log(desc);
 
-	T.post('statuses/update', { status: "The weather in " + city + " is " + temp + "F and " + desc + "." }, function(err, data, response) {
+	T.post('statuses/update', { status: "The weather in " + thecity + " is " + temp + "F and " + desc + "." }, function(err, data, response) {
   if(err) {
     console.log("There was a problem tweeting the message.", err);
   }
